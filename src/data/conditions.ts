65c6578cc017d7e719bc55a0d328574c593a6914
Volodymyr <vnechyporenko@prosperityehr.com>
2025-03-13 22:06:47 +0200

    Adds images

---
+++ src/data/conditions.ts
@@ -0,0 +1,8 @@
+export const careConditions = [
+  'care-conditions-01.jpeg',
+  'care-conditions-02.jpeg',
+  'care-conditions-03.jpeg',
+  'care-conditions-04.jpeg',
+  'care-conditions-05.jpeg',
+  'care-conditions-06.jpeg',
+];
--- src/hooks/useRandomImages.ts
+++ src/hooks/useRandomImages.ts
@@ -1,32 +1,15 @@
-import accessories from '@data/accessories';
-import kitchen from '@data/kitchen';
-import pillows from '@data/pillows';
-import toys from '@data/toys';
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { shuffleArray } from '@utils/shuffleArray';
-import { ImagePaths } from '@interfaces/interfaces';
+import { careConditions } from '../data/conditions';
 
 const useRandomImages = () => {
   const [imageSet, setImageSet] = useState<string[]>([]);
 
-  const arrayToShuffle = useMemo(() => {
-    const dataArrays: ImagePaths[][] = [
-      accessories,
-      kitchen,
-      pillows,
-      toys,
-    ];
-
-    return dataArrays.flatMap((array) =>
-      array.slice(-3).map((item) => item.images[0])
-    );
+  useEffect(() => {
+    setImageSet(shuffleArray(careConditions));
   }, []);
 
-  useEffect(() => {
-    setImageSet(shuffleArray(arrayToShuffle))
-  }, [arrayToShuffle]);
-
-  return imageSet
-}
+  return imageSet;
+};
 
 export default useRandomImages;
